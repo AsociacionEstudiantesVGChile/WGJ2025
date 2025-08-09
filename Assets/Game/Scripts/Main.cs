3e4309b39e1c057b8da4b04ee899e8eeb7dfa6aa
sistemas <sistemas@SPO_LAB50917.inacap.cl>
2025-08-09 12:44:21 -0400

    falmy descarga esto

--- Assets/Game/Scripts/Main.cs
+++ Assets/Game/Scripts/Main.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class Main : MonoBehaviour {
 
-    
+    [SerializeField] private DayInfo[] _days;
+
+    private void Start() {
+        foreach (DayInfo day in _days) {
+            
+        }
+    }
 }
